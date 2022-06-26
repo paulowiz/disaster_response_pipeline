fc2da56b295f697a03e1512bf03d8ca6985392db
Paulo Mota <phmota@outlook.com.br>
2022-06-26 11:44:42 +0200

    Final files working update

---
+++ data/process_data.py
@@ -0,0 +1,72 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    # Merge dataframes
+    df = messages.merge(categories, left_on='id', right_on='id')
+    return df
+
+
+def clean_data(df):
+    # Grab the disaster categories and separate in individual tables
+    categories =  df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    row = [x.split('-')[0] for x in row]
+    category_colnames = row
+    categories.columns = category_colnames
+
+    # Clean the categories columns to let just the number
+    for column in categories:
+        categories[column] = [x.split('-')[-1] for x in categories[column]]
+        categories[column] = categories[column].astype(int)
+
+    # drop old categories' column from the main dataframe
+    df.drop(['categories'], axis=1, inplace = True)
+
+    # Add id column to the new categories dataframe to merge them
+    categories['id'] = df['id']
+    df = df.merge(categories, left_on='id', right_on='id')
+
+    # Drop duplicates
+    df = df.drop_duplicates()
+    return df
+
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('disaster_message', engine, index=False)
+    pass  
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
